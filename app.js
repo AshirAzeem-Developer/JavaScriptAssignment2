8867f8ff6c5d2cc6a53eaedeaf16bee669572b17
Ashir Azeem <ashir9112001@gmail.com>
2022-11-05 08:08:39 +0500

    Assignment Complete

---
+++ app.js
@@ -0,0 +1,152 @@
+// -------------->>>>>CHAPTER 09<<<<<<<----------
+// var spec = prompt("Your species?", "human");
+//  var question = "Your species?";
+//  var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+// var numberOfCats = prompt("How many cats?");
+// var tooManyCats = numberOfCats + 1;
+// console.log(tooManyCats);
+// -------------->>>>>CHAPTER 10<<<<<<<----------
+
+//  var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+
+//  var correctAnswer = "Vatican";
+//  if (x === correctAnswer) {
+//  alert("Correct!");
+//  }
+var score = 0;
+//  var correctAnswer = "Vatican";
+//  if (x === correctAnswer) {
+//  score++;
+//  console.log(score);
+//  userIQ = "genius";
+//  alert("Correct!");
+//  }
+
+// -------------->>>>>CHAPTER 11<<<<<<<----------
+
+//All of the Examples written below are Comparing the Data as Well as DataType
+
+// if (fullName === "Mark" + " " + "Myers") {
+// if (fullName === firstName + " " + "Myers") {
+// if (fullName === firstName + " " + "Myers") {
+// if (fullName === "firstName + " " + lastName) {
+// if (totalCost === 81.50 + 135) {
+// if (totalCost === materialsCost + 135) {
+// if (totalCost === materialsCost + laborCost) {
+// if (x + y === a - b) {
+
+// var yourTicketNumber= +prompt("Enter Your ticket Number?")
+// if (yourTicketNumber !== 487208) {
+
+//     alert("Better luck next time.");
+// }
+// else{
+//     alert("Congrats!!!.");
+// }
+
+// -------------->>>>>CHAPTER 12<<<<<<<----------
+
+//  var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+
+//  var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+//  if (x !== "Vatican") {
+//  alert("Wrong answer");
+//  }
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+//  else {
+//  alert("Wrong answer");
+//  }
+
+
+//  var correctAnswer = "Vatican";
+//  if (x === correctAnswer) {
+//  alert("Correct!");
+//  }
+//  else {
+//  score--;
+//  userIQ = "problematic";
+//  console.log(score);
+//  alert("Incorrect");
+//  }
+
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+//     alert("Correct!");
+// }
+// else if (x === "Rome") {
+//     alert("Incorrect but close");
+// }
+// else {
+//     alert("Incorrect");
+// }
+
+// -------------->>>>>CHAPTER 13<<<<<<<----------
+// var weight= +prompt("Enter Weight?")
+// var time= +prompt("Enter Time?")
+
+//  if (weight > 300 && time < 6) {
+//  alert("Come to our tryout!");
+//  }
+//  else {
+//  alert("Come to our cookout!");
+//  }
+
+//  if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//  alert("Come to our tryout!");
+//  }
+//  else {
+//  alert("Come to our cookout!");
+//  }
+
+// var GPA= +prompt("Enter GPA?")
+// var sport= prompt("Which sport Do You play?")
+//  if ( GPA > 2.5 || sport === "football") {
+//  alert("Welcome to Bubba State!");
+//  }
+//  else {
+//  alert("Have you looked into appliance repair?");
+//  }
+
+// -------------->>>>>CHAPTER 14<<<<<<<----------
+
+// var x =1;
+// var y =1;
+// var a =1;
+// var b =1;
+// var c =2;
+// var d =2;
+
+//  if ((x === y || a === b) && c === d) {
+//  g = h;
+//  }
+//  else {
+//  e = f;
+//  }
+
+// if (c === d) {
+//     if (x === y) {
+//         g = h;
+//     }
+//     else if (a === b) {
+//         g = h;
+//     }
+//     else {
+//         e = f;
+//     }
+// }
+// else {
+//     e = f;
+// }
+
